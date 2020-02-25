c896cb20c5853d7d01d1d7e1eee231a6e8a5077a
theodorkudryk <54710389+theodorkudryk@users.noreply.github.com>
2020-02-25 22:35:43 +0100

    Add files via upload

--- skapa.h
+++ skapa.h
@@ -10,11 +10,25 @@ char * itoaconv( int num );
 #define BUTTON2 ((PORTD >> 6) & 0x1)
 #define BUTTON3 ((PORTD >> 5) & 0x1)
 
+extern uint8_t car_heights[8];
+extern uint8_t car_width[8];
 
-/*
- //Declare text buffer for display output
+extern uint8_t obstacle_heights[8];
+extern uint8_t obstacle_width[8];
+
+void display_image(int x, const uint8_t *data);
+void display_init(void);
+void display_string(int line, char *s);
+void display_update(void);
+uint8_t spi_send_recv(uint8_t data);
+void quicksleep(int cyc);
+
+extern const uint8_t const font[128*8];
+extern const uint8_t const car[8];
+const uint8_t const obstacle[8];
 extern char textbuffer[4][16];
-
+// Clear whole screen
+void screen_clear(void);
 
 // Game variables
 int running;
